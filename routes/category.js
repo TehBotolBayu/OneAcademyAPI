8214a0628607ef2581ac04d87117d813730fbb0d
dimasz555 <h1101211047@student.untan.ac.id>
2023-11-29 20:23:49 +0700

    fix route and controller

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,6 @@
 const express = require("express"),
     router = express.Router(),
-    controller = require('../controllers/category.controller')
+    controller = require('../controllers/category.controller'),
     multerLib = require('multer')();
 
 
