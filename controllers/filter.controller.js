31fe78a2f9e478f50535bd937a483ec0eb0a1d72
rakkuyyy <rakkuyyy@gmail.com>
2023-12-03 20:37:24 +0700

    update filter controller

--- controllers/filter.controller.js
+++ controllers/filter.controller.js
@@ -3,133 +3,198 @@ const { Courses } = require("../models");
 module.exports = {
   filter: async (req, res) => {
     try {
-      const { sortBy, filterBy, queryParam } = req.query;
+      const { sortBy, category, level, promo } = req.query;
 
-      let courses;
-
-      // Define default sorting
-      const defaultSort = { updatedAt: "desc" };
-
-      // Define filters based on filterBy parameter
       const filters = {
-        newest: { orderBy: { updatedAt: "desc" } },
-        oldest: { orderBy: { updatedAt: "asc" } },
-        promo: { where: { price: { lt: 50 } } }, // Adjust the condition for your promotional pricing
+        newest: { createdAt: "desc" },
+        oldest: { createdAt: "asc" },
+        promo: { price: { lt: 50 } }, // Sesuaikan kondisi promo sesuai kebutuhan
       };
 
-      // Apply filters based on filterBy value
-      const filter = filters[filterBy] || {};
-
-      courses = await Courses.findMany({
-        where: {
-          OR: [
-            { title: { contains: queryParam || "" } },
-            { instructor: { contains: queryParam || "" } },
-            { description: { contains: queryParam || "" } },
-            { level: { contains: queryParam || "" } },
-            // Add more fields to search as needed
-          ],
-          ...filter,
-        },
-        orderBy: sortBy ? { [sortBy]: "desc" } : defaultSort,
-        include: {
-          category: true,
-          image: true,
-          material: true,
-          transaction: true,
-          review: true,
-        },
-      });
-
-      res.json(courses);
-    } catch (error) {
-      res.status(500).json({ error: "Tidak ditemukan" });
-    }
-  },
-
-  categoryFilter: async (req, res) => {
-    try {
-      const { queryParam, category } = req.query;
-
-      let courses;
-
-      const filters = {
-        "UI/UX Designer": { categoryId: "Isi Id nya UI/UX Design" },
-        "Web Development": {
-          categoryId: "Isi Id nya Web Development",
-        },
-        "Android Development": {
-          categoryId: "Isi Id nya Android Development",
-        },
-        "Data Science": { categoryId: "Isi Id nya Data Science" },
+      const categoryFilters = {
+        "UI/UX Design": { category: { name: "UI/UX Design" } },
+        "Web Development": { category: { name: "Web Development" } },
+        "Android Development": { category: { name: "Android Development" } },
+        "Data Science": { category: { name: "Data Science" } },
         "Business Intelligence": {
-          categoryId: " Isi Id nya Business Intelligence",
+          category: { name: "Business Intelligence" },
         },
-        // Add more categories as needed
       };
 
-      const filter = filters[category];
+      const levelFilters = {
+        all: {},
+        beginner: { level: "Beginner" },
+        intermediate: { level: "Intermediate" },
+        advanced: { level: "Advanced" },
+      };
 
-      courses = await Courses.findMany({
-        where: {
-          AND: [
-            {
-              OR: [
-                { title: { contains: queryParam || "" } },
-                { instructor: { contains: queryParam || "" } },
-                { description: { contains: queryParam || "" } },
-                { level: { contains: queryParam || "" } },
-                // Add more fields to search as needed
-              ],
-            },
-            filter, // Apply category filter
-          ],
-        },
+      let query = {
         include: {
           category: true,
           image: true,
           material: true,
           transaction: true,
           review: true,
+          // chapter: true,
         },
-      });
+      };
 
-      res.json(courses);
-    } catch (error) {
-      res.status(500).json({ error: "Something went wrong" });
-    }
-  },
-
-  levelFilter: async (req, res) => {
-    try {
-      const { level } = req.query;
-      let courses;
-
-      if (level) {
-        courses = await Courses.findMany({
-          where: {
-            level: {
-              equals: level.toLowerCase(), // Memastikan level dalam format lowercase di database
-            },
-          },
-          include: {
-            // Include apapun yang ingin kamu kembalikan dalam respons
-            // Misalnya, jika ingin memasukkan informasi kategori kursus:
-            category: true,
-          },
-        });
+      if (sortBy) {
+        query.orderBy = filters[sortBy];
       } else {
-        // Jika tidak ada filter level yang diberikan, kembalikan semua kursus
-        courses = await Courses.findMany({
-          include: {
-            category: true,
-          },
-        });
+        query.orderBy = filters.newest;
       }
 
+      if (category) {
+        query.where = { ...query.where, ...categoryFilters[category] };
+      }
+      if (level) {
+        query.where = { ...query.where, ...levelFilters[level] };
+      }
+      if (promo && filters.promo) {
+        query.where = { ...query.where, ...filters.promo };
+      }
+
+      const courses = await Courses.findMany(query);
       res.json(courses);
     } catch (error) {
-      res.status(500).json({ error: "Terjadi kesalahan dalam server." });
+      console.error("Terjadi kesalahan:", error);
+      // console.error(error);
+      res.status(500).json({ error: "Terjadi kesalahan" });
     }
   },
 };
+
+// module.exports = {
+//   filter: async (req, res) => {
+//     try {
+//       const { sortBy, filterBy, queryParam } = req.query;
+
+//       let courses;
+
+//       // Define default sorting
+//       const defaultSort = { updatedAt: "desc" };
+
+//       // Define filters based on filterBy parameter
+//       const filters = {
+//         newest: { orderBy: { updatedAt: "desc" } },
+//         oldest: { orderBy: { updatedAt: "asc" } },
+//         promo: { where: { price: { lt: 50 } } }, // Adjust the condition for your promotional pricing
+//       };
+
+//       // Apply filters based on filterBy value
+//       const filter = filters[filterBy] || {};
+
+//       courses = await Courses.findMany({
+//         where: {
+//           OR: [
+//             { title: { contains: queryParam || "" } },
+//             { instructor: { contains: queryParam || "" } },
+//             { description: { contains: queryParam || "" } },
+//             { level: { contains: queryParam || "" } },
+//             // Add more fields to search as needed
+//           ],
+//           ...filter,
+//         },
+//         orderBy: sortBy ? { [sortBy]: "desc" } : defaultSort,
+//         include: {
+//           category: true,
+//           image: true,
+//           material: true,
+//           transaction: true,
+//           review: true,
+//         },
+//       });
+
+//       res.json(courses);
+//     } catch (error) {
+//       res.status(500).json({ error: "Tidak ditemukan" });
+//     }
+//   },
+
+//   categoryFilter: async (req, res) => {
+//     try {
+//       const { queryParam, category } = req.query;
+
+//       let courses;
+
+//       const filters = {
+//         "UI/UX Designer": { categoryId: "Isi Id nya UI/UX Design" },
+//         "Web Development": {
+//           categoryId: "Isi Id nya Web Development",
+//         },
+//         "Android Development": {
+//           categoryId: "Isi Id nya Android Development",
+//         },
+//         "Data Science": { categoryId: "Isi Id nya Data Science" },
+//         "Business Intelligence": {
+//           categoryId: " Isi Id nya Business Intelligence",
+//         },
+//         // Add more categories as needed
+//       };
+
+//       const filter = filters[category];
+
+//       courses = await Courses.findMany({
+//         where: {
+//           AND: [
+//             {
+//               OR: [
+//                 { title: { contains: queryParam || "" } },
+//                 { instructor: { contains: queryParam || "" } },
+//                 { description: { contains: queryParam || "" } },
+//                 { level: { contains: queryParam || "" } },
+//                 // Add more fields to search as needed
+//               ],
+//             },
+//             filter, // Apply category filter
+//           ],
+//         },
+//         include: {
+//           category: true,
+//           image: true,
+//           material: true,
+//           transaction: true,
+//           review: true,
+//         },
+//       });
+
+//       res.json(courses);
+//     } catch (error) {
+//       res.status(500).json({ error: "Something went wrong" });
+//     }
+//   },
+
+//   levelFilter: async (req, res) => {
+//     try {
+//       const { level } = req.query;
+//       let courses;
+
+//       if (level) {
+//         courses = await Courses.findMany({
+//           where: {
+//             level: {
+//               equals: level.toLowerCase(), // Memastikan level dalam format lowercase di database
+//             },
+//           },
+//           include: {
+//             // Include apapun yang ingin kamu kembalikan dalam respons
+//             // Misalnya, jika ingin memasukkan informasi kategori kursus:
+//             category: true,
+//           },
+//         });
+//       } else {
+//         // Jika tidak ada filter level yang diberikan, kembalikan semua kursus
+//         courses = await Courses.findMany({
+//           include: {
+//             category: true,
+//           },
+//         });
+//       }
+
+//       res.json(courses);
+//     } catch (error) {
+//       res.status(500).json({ error: "Terjadi kesalahan dalam server." });
+//     }
+//   },
+// };
--- routes/filter.js
+++ routes/filter.js
@@ -5,10 +5,12 @@ const express = require("express"),
 router.get("/filterSearch", filterController.filter);
 // localhost:3000/api/v1/filter/filterSearch?filterBy=newest
 
-router.get("filterCategory", filterController.categoryFilter);
-// localhost:3000/api/v1/filter/filterCategory?category=UI/UX Designer
+// localhost:3000/api/v1/filter/filterSearch?sortBy=newest&category=Web%20Development&level=beginner&promo=true
 
-router.get("filterLevel", filterController.levelFilter);
-// localhost:3000/api/v1/filter/filterLevel?
+// router.get("filterCategory", filterController.categoryFilter);
+// // localhost:3000/api/v1/filter/filterCategory?category=UI/UX Designer
+
+// router.get("filterLevel", filterController.levelFilter);
+// // localhost:3000/api/v1/filter/filterLevel?
 
 module.exports = router;
