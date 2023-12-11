17c6e51b35f1f4560cb4f67794aa1e4aad07c75d
fathurjoy <fathurmohammad657@students.amikom.ac.id>
2023-12-11 19:20:27 +0700

    User Profile

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,363 +1,355 @@
-const {Users, Profiles, Roles} = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
-
+const { Users, Profiles, Roles } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 async function hashPassword(plaintextPassword) {
-    const hash = await bcrypt.hash(plaintextPassword, 10);
-    return hash;
+  const hash = await bcrypt.hash(plaintextPassword, 10);
+  return hash;
 }
 
 const cryptPassword = async (password) => {
-    const salt = await bcrypt.genSalt(5);
-    return bcrypt.hash(password, salt)
-}
+  const salt = await bcrypt.genSalt(5);
+  return bcrypt.hash(password, salt);
+};
 
 function generateOTP() {
-    var digits = '0123456789'; 
-    let OTP = ''; 
-    for (let i = 0; i < 6; i++ ) { 
-        OTP += digits[Math.floor(Math.random() * 10)]; 
-    } 
-    return OTP; 
-} 
+  var digits = "0123456789";
+  let OTP = "";
+  for (let i = 0; i < 6; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+}
 
 module.exports = {
-    getAllRole: async (req, res) => {
-        try {
-            const roles = await Roles.findMany()
-            return res.status(201).json({
-                roles
-            })
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({ error: "Something went wrong" });
+  getAllRole: async (req, res) => {
+    try {
+      const roles = await Roles.findMany();
+      return res.status(201).json({
+        roles,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  },
+
+  createRole: async (req, res) => {
+    try {
+      const role = await Roles.create({
+        data: {
+          name: req.body.name,
+        },
+      });
+      return res.status(201).json({
+        role,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  },
+
+  register: async (req, res) => {
+    try {
+      let date = new Date();
+      date.setMinutes(date.getMinutes() + 5);
+      date.toISOString();
+      const hashed = await hashPassword(req.body.password);
+      const user = await Users.create({
+        data: {
+          email: req.body.email,
+          phone: req.body.phone,
+          password: hashed,
+          codeOTP: generateOTP(),
+          OTPlimit: date,
+          status: "inactive",
+          role: {
+            connect: { id: parseInt(req.body.roleId) },
+          },
+        },
+      });
+
+      const profile = await Profiles.create({
+        data: {
+          name: req.body.name,
+          user: {
+            connect: { id: user.id },
+          },
+        },
+      });
+
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: "system@gmail.com",
+        to: req.body.email,
+        subject: "Account Verification",
+        html: `<p>Your OTP</p><h1>${user.codeOTP}</h1>`,
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(400);
         }
-    },
+        return res.status(200).json({
+          message: "account is created, OTP sent",
+          user,
+        });
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(400).json({
+        error,
+      });
+    }
+  },
 
-    createRole: async (req, res) => {
-        try {
-            const role = await Roles.create({
-                data: {
-                    name: req.body.name
-                }
-            })
-            return res.status(201).json({
-                role
-            })
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({ error: "Something went wrong" });
+  verifyOTP: async (req, res) => {
+    try {
+      const acc = await Users.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (req.body.OTP == acc.codeOTP) {
+        let now = new Date().toISOString();
+        if (acc.OTPlimit.toISOString() > now) {
+          await Users.update({
+            data: {
+              codeOTP: null,
+              OTPlimit: null,
+              status: "active",
+            },
+            where: {
+              email: req.body.email,
+            },
+          });
+
+          return res.status(200).json({
+            message: "Your Account has activated",
+          });
+        } else {
+          return res.status(400).json({
+            message: "Your OTP is expired, try resetting it",
+          });
         }
-    },
+      } else {
+        return res.status(200).json({
+          message: "Your OTP is invalid",
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(400).json({
+        error,
+      });
+    }
+  },
 
-    register: async (req, res) => {
-        try {
-            let date = new Date();
-            date.setMinutes(date.getMinutes() + 5);
-            date.toISOString();
-            const hashed = await hashPassword(req.body.password);
-            const user = await Users.create({
-                data: {
-                    email: req.body.email,
-                    phone: req.body.phone,
-                    password: hashed,
-                    codeOTP: generateOTP(),
-                    OTPlimit: date,
-                    status: "inactive",
-                    role: {
-                        connect: {id: parseInt(req.body.roleId)}
-                    }
-                }
-            });
-        
-            const profile = await Profiles.create({
-                data: {
-                    name: req.body.name,
-                    user: {
-                        connect: {id: user.id}
-                    }
-                }
-            });
+  resetOTP: async (req, res) => {
+    try {
+      let date = new Date();
+      date.setMinutes(date.getMinutes() + 5);
+      date.toISOString();
+      const acc = await Users.update({
+        data: {
+          codeOTP: generateOTP(),
+          OTPlimit: date,
+        },
+        where: {
+          email: req.body.email,
+        },
+      });
 
-            const transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
 
-            const mailOptions = {
-                from: 'system@gmail.com',
-                to : req.body.email,
-                subject: "Account Verification",
-                html: `<p>Your OTP</p><h1>${user.codeOTP}</h1>`
-            }
+      const mailOptions = {
+        from: "system@gmail.com",
+        to: req.body.email,
+        subject: "Account Verification",
+        html: `<p>Your OTP</p><h1>${acc.codeOTP}</h1>`,
+      };
 
-            transporter.sendMail(mailOptions, (err) => {
-                if(err) {
-                    console.log(err)
-                    return res.status(400);
-                }
-                return res.status(200).json({
-                    message: "account is created, OTP sent",
-                    user
-                });
-            })
-        
-            
-        } catch (error) {
-            console.log(error.message);
-            return res.status(400).json({
-                error
-            })
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(400);
         }
-    },
+        return res.status(200).json({
+          message: "We have sent a new OTP, check your email",
+        });
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(400).json({
+        error,
+      });
+    }
+  },
 
-    verifyOTP: async (req, res) => {
-        try {
-            const acc = await Users.findUnique({
-                where: {
-                    email: req.body.email
-                }
-            })
-            
-            if(req.body.OTP == acc.codeOTP){
-                let now = new Date().toISOString();
-                if(acc.OTPlimit.toISOString() > now){
-                    await Users.update({
-                        data: {
-                            codeOTP: null,
-                            OTPlimit: null,
-                            status: "active"
-                        },
-                        where: {
-                            email: req.body.email
-                        }
-                    })
+  login: async (req, res) => {
+    try {
+      const findUser = await Users.findFirst({
+        where: {
+          email: req.body.email,
+        },
+      });
 
-                    return res.status(200).json({
-                        message: "Your Account has activated"
-                    })
-                } else {
-                    return res.status(400).json({
-                        message: "Your OTP is expired, try resetting it"
-                    })
-                }
-            } else {
-                return res.status(200).json({
-                    message: "Your OTP is invalid"
-                })
-            }
-        } catch (error) {
-            console.log(error.message);
-            return res.status(400).json({
-                error
-            })
+      if (!findUser) {
+        return res.status(404).json({
+          error: "Email tidak terdaftar!",
+        });
+      }
+
+      if (findUser.status === "inactive") {
+        return res.status(401).json({
+          message: "Account is not activated, please enter OTP",
+        });
+      }
+
+      if (bcrypt.compareSync(req.body.password, findUser.password)) {
+        const token = jwt.sign(
+          {
+            id: findUser.id,
+          },
+          "secret_key",
+          {
+            expiresIn: "24h",
+          },
+        );
+        return res.status(200).json({
+          data: {
+            token,
+          },
+          id: findUser.id,
+        });
+      }
+
+      return res.status(403).json({
+        error: "Invalid credentials",
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).json({
+        error,
+      });
+    }
+  },
+
+  resetPassword: async (req, res) => {
+    try {
+      const findUser = await Users.findFirst({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (!findUser) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      }
+
+      const encrypt = await cryptPassword(req.body.email);
+
+      await Users.update({
+        data: {
+          resetToken: encrypt,
+        },
+        where: {
+          id: findUser.id,
+        },
+      });
+
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: "system@gmail.com",
+        to: req.body.email,
+        subject: "Reset Password",
+        html: `<p>Reset Password </p><a href="localhost:5000/set-password/${encrypt}">Click Here</a><br></br><p>Paste this url to your browser if you cant click link above</p><p>localhost:5000/set-password/${encrypt}</p>`,
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(400).json({
+            message: "Something went wrong",
+          });
         }
-    },
 
-    resetOTP: async (req, res) => {
-        try {
-            let date = new Date();
-            date.setMinutes(date.getMinutes() + 5);
-            date.toISOString();
-            const acc = await Users.update({
-                data:{
-                    codeOTP: generateOTP(),
-                    OTPlimit: date
-                },
-                where: {
-                    email: req.body.email
-                }
-            })
+        return res.status(200).json({
+          message: "email sent",
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error,
+      });
+    }
+  },
 
-            const transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
+  setPassword: async (req, res) => {
+    try {
+      const findUser = await Users.findFirst({
+        where: {
+          resetToken: req.body.key,
+        },
+      });
 
-            const mailOptions = {
-                from: 'system@gmail.com',
-                to : req.body.email,
-                subject: "Account Verification",
-                html: `<p>Your OTP</p><h1>${acc.codeOTP}</h1>`
-            }
+      if (!findUser) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      }
 
-            transporter.sendMail(mailOptions, (err) => {
-                if(err) {
-                    console.log(err)
-                    return res.status(400);
-                }
-                return res.status(200).json({
-                    message: "We have sent a new OTP, check your email",
-                });
-            })
+      await Users.update({
+        data: {
+          password: await cryptPassword(req.body.password),
+          resetToken: null,
+        },
+        where: {
+          id: findUser.id,
+        },
+      });
 
-        } catch (error) {
-            console.log(error.message);
-            return res.status(400).json({
-                error
-            })
-        }
-    },
-
-    login: async (req, res) => {
-        try {
-            const findUser = await Users.findFirst({
-                where: {
-                    email: req.body.email
-                }
-            })
-        
-            if(!findUser){
-                return res.status(404).json({
-                    error: 'Email tidak terdaftar!'
-                });
-            }
-
-            if(findUser.status === "inactive"){
-                return res.status(401).json({
-                    message: "Account is not activated, please enter OTP"
-                })
-            }
-        
-            if(bcrypt.compareSync(req.body.password, findUser.password)){
-                const token = jwt.sign(
-                {
-                    id: findUser.id
-                },
-                'secret_key', 
-                {
-                    expiresIn: '24h'
-                })
-                return res.status(200).json({
-                    data: {
-                        token
-                    },
-                    id: findUser.id
-                })
-            }
-        
-            return res.status(403).json({
-                error: 'Invalid credentials'
-            })
-        } catch (error) {
-            console.log(error.message);
-            res.status(400).json({
-                error
-            })
-        }
-    },
-
-    resetPassword: async (req, res) => {
-        try {
-            const findUser = await Users.findFirst({
-                where: {
-                    email: req.body.email
-                }
-            });
-
-            if(!findUser) {
-                return res.status(400).json({
-                    message: "User not found"
-                });
-            }
-
-            const encrypt = await cryptPassword(req.body.email);
-
-            await Users.update({
-                data: {
-                    resetToken: encrypt,
-                },
-                where: {
-                    id: findUser.id
-                }
-            });
-
-            const transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
-
-            const mailOptions = {
-                from: 'system@gmail.com',
-                to : req.body.email,
-                subject: "Reset Password",
-                html: `<p>Reset Password </p><a href="localhost:5000/set-password/${encrypt}">Click Here</a><br></br><p>Paste this url to your browser if you cant click link above</p><p>localhost:5000/set-password/${encrypt}</p>`
-            }
-
-            transporter.sendMail(mailOptions, (err) => {
-                if(err) {
-                    console.log(err)
-                    return res.status(400).json({
-                        message: "Something went wrong"
-                    });
-                }
-
-                return res.status(200).json({
-                    message: "email sent"
-                });
-            })
- 
-            
-            
-        } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                error
-            });
-        }
-    },
-
-    setPassword: async (req, res) => {
-        try {
-            
-            const findUser = await Users.findFirst({
-                where: {
-                    resetToken: req.body.key
-                }
-            });
-
-            if(!findUser) {
-                return res.status(400).json({
-                    message: "User not found"
-                });
-            }
-
-            await Users.update({
-                data: {
-                    password: await cryptPassword(req.body.password),
-                    resetToken: null
-                },
-                where: {
-                    id: findUser.id
-                }
-            });
-
-            return res.status(200).json({
-                message: "Password has changed"
-            });;
-            
-        } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                error
-            });
-        }
-    },
-}
+      return res.status(200).json({
+        message: "Password has changed",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error,
+      });
+    }
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,29 @@
-const express = require('express'),
-    router = express.Router(),
-    controller = require('../controllers/auth.controller.js');
+const express = require("express"),
+  router = express.Router(),
+  controller = require("../controllers/auth.controller.js"),
+  userController = require("../controllers/user.controller.js"),
+  multerLib = require("multer")(),
+  auth = require("../middlewares/auth.js");
 
-router.post('/register', controller.register);
-router.post('/login', controller.login);
-router.post('/verify', controller.verifyOTP);
-router.post('/resetOTP', controller.resetOTP);
-router.post('/reset-password', controller.resetPassword);
-router.post('/set-password', controller.setPassword);
+router.post("/register", controller.register);
+router.post("/login", controller.login);
+router.post("/verify", controller.verifyOTP);
+router.post("/resetOTP", controller.resetOTP);
+router.post("/reset-password", controller.resetPassword);
+router.post("/set-password", controller.setPassword);
 // router.get('/', userController.getUsers);
 // router.get('/:userId', userController.getUserById, imageKit.imagekitGet);
 // router.delete('/delete/:userId', auth.checkToken, userController.deleteUser, imageKit.imagekitDelete);
 // router.put('/updateProfile/:userId', auth.checkToken, multer.single('image'), imageKit.imagekitUpload, userController.updateProfile);
 // router.put('/updateUser/:userId', auth.checkToken, multer.single('image'), userController.updateUser);
+
+router.get("/me", auth.checkToken, userController.getUserById);
+router.put("/me/change-password", auth.checkToken, userController.changePwd);
+router.put(
+  "/me",
+  auth.checkToken,
+  multerLib.single("avatar"),
+  userController.updateUserById,
+);
 
 module.exports = router;
