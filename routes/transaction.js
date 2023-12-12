9e157e7e66c583bb5238e0b9735629f3cbbda180
dimasz555 <h1101211047@student.untan.ac.id>
2023-12-12 19:03:57 +0700

    fix bug template email

--- routes/transaction.js
+++ routes/transaction.js
@@ -8,5 +8,6 @@ const express = require("express"),
 router.post("/:id/buy", middleware.checkToken, controller.buyCourse);
 router.get("/:id/detailTransaction",middleware.checkToken,controller.getTransaction);
 router.post("/:id/pay", middleware.checkToken, controller.payTransaction);
+router.post("/", controller.getAllTransaction);
 
 module.exports = router;
