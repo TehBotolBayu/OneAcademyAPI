a149e5368620ad16d078ebbc089a2a164b926157
dimasz555 <h1101211047@student.untan.ac.id>
2023-12-25 19:49:15 +0700

    add api docs

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.use(express.json({ strict: false }));
 app.use(cors());
 app.use("/api/v1", router);
 
+app.get("/api/docs", (req, res, next) => {
+  try {
+      res.redirect(
+          "https://documenter.getpostman.com/view/24198059/2s9YkrbfXL"
+      );
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error : "Something went wrong" });
+  }
+});
+
 app.get("*", (req, res) => {
   return res.status(404).json({
     error: "End point is not registered",
